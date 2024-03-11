9a54c1726dc6e34870cc022c0138bbf9a9068114
Funwi Kelsea <funwikelseandohnwi@gmail.com>
2024-03-11 10:19:59 +0100

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import "package:weather_project/pages/weather_app.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+// we always make our app to be a stateless widget
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(
+        useMaterial3: true,
+      ).copyWith(
+        appBarTheme: const AppBarTheme()
+      ),
+      home: const WeatherApp(),
+    );
+  }
+}
